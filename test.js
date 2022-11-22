64a1c88442dc24d15e287da81e63701207afedb5
Vinay Sajip <vinay_sajip@yahoo.co.uk>
2022-11-22 07:15:30 +0000

    Add debug statement for CI.

--- test.js
+++ test.js
@@ -1644,6 +1644,7 @@ describe('Config', function () {
 
     plist.forEach(function(p) {
       const s = "test: @'foo'".replace(/foo/, p.replace('\\', '/'));
+      console.log(s);
       const cfg = new Config(makeStream(s));
       assert.equal(cfg.get('test.computed6'), 2);
     });
